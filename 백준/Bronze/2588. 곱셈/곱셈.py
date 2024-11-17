7d8c375cfb2bf50b0d71469b3e5b5bdb9e74be2f
aing35 <vhrfuf010@naver.com>
2024-11-17 23:42:29 +0900

    [Bronze III] Title: 곱셈, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,13 @@
+a = input()
+b = input()
+
+ab_3 = int(a) * int(b[2])
+ab_4 = int(a) * int(b[1])
+ab_5 = int(a) * int(b[0])
+
+ab_6 = ab_3 + 10 * ab_4 + 100 * ab_5
+
+print(f"{ab_3}")
+print(f"{ab_4}")
+print(f"{ab_5}")
+print(f"{ab_6}")
